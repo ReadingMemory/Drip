f74bf3a231f8677c3550165fd5c403316dddccbb
ReadingMemory <adamomoush00@gmail.com>
2025-03-23 17:06:13 +0800

    Update Init.lua

--- Init.lua
+++ Init.lua
@@ -83,7 +83,7 @@ warn("init loaded")
 
 getgenv().RBXActive = true
 getgenv().invalidated = {}
-getgenv().Files
+getgenv().Files = {}
 
 local nilinstances
 game.DescendantRemoving:Connect(function(d)
@@ -102,7 +102,7 @@ getinfo = newcclosure(function(f, options)
 	else
 		options = "sflnu"
 	end
-    
+
 	local result = {}
 	for index = 1, #options do
 		local option = string.sub(options, index, index)
@@ -142,20 +142,20 @@ end)
 ]]
 
 getgenv()["debug"] = {
-    ["getinfo"] = getinfo,
-    ["traceback"] = debug["traceback"],
-    ["info"] = debug["info"],
-    ["profilebegin"] = debug["profilebegin"],
-    ["profileend"] = debug["profileend"],
-    ["getmemorycategory"] = debug["getmemorycategory"],
-    ["setmemorycategory"] = debug["setmemorycategory"],
-    ["resetmemorycategory"] = debug["resetmemorycategory"],
-    ["dumpcodesize"] = debug["dumpcodesize"],
+	["getinfo"] = getinfo,
+	["traceback"] = debug["traceback"],
+	["info"] = debug["info"],
+	["profilebegin"] = debug["profilebegin"],
+	["profileend"] = debug["profileend"],
+	["getmemorycategory"] = debug["getmemorycategory"],
+	["setmemorycategory"] = debug["setmemorycategory"],
+	["resetmemorycategory"] = debug["resetmemorycategory"],
+	["dumpcodesize"] = debug["dumpcodesize"],
 }
 
 getgenv().cache = {
-    invalidate = newcclosure(function(object)
-        local function clone(object)
+	invalidate = newcclosure(function(object)
+		local function clone(object)
 			local old_archivable = object.Archivable
 			local clone
 
@@ -173,16 +173,16 @@ getgenv().cache = {
 
 		object:Destroy()
 		clone.Parent = oldParent
-    end),
-    iscached = newcclosure(function(object)
-        return table.find(invalidated, object) == nil
-    end),
-    replace = newcclosure(function(object, new_object)
-        if object:IsA("BasePart") and new_object:IsA("BasePart") then
+	end),
+	iscached = newcclosure(function(object)
+		return table.find(invalidated, object) == nil
+	end),
+	replace = newcclosure(function(object, new_object)
+		if object:IsA("BasePart") and new_object:IsA("BasePart") then
 			invalidate(object)
 			table.insert(invalidated, new_object)
 		end
-    end)
+	end)
 }
 
 getgenv().getrunningscripts = newcclosure(function()
@@ -197,24 +197,24 @@ end)
 
 
 getgenv().getscripthash = newcclosure(function(script)
-    assert(typeof(scr) == 'Instance', 'Argument #1 to \'getscripthash\' must be an Instance, not ' .. typeof(scr))
+	assert(typeof(scr) == 'Instance', 'Argument #1 to \'getscripthash\' must be an Instance, not ' .. typeof(scr))
 	assert(scr.ClassName ~= 'LocalScript' or scr.ClassName ~= 'Script', 'Argument #1 to \'getscripthash\' must be a LocalScript or Script')
 
 	return script:GetHash()
 end)
 
 getgenv().getscripts = newcclosure(function()
-    local returntable = {}
-    for i, v in pairs(game:GetDescendants()) do
-        if v:IsA("LocalScript") or v:IsA("ModuleScript") then
-            table.insert(returntable, v)
-        end
-    end
-    return returntable
+	local returntable = {}
+	for i, v in pairs(game:GetDescendants()) do
+		if v:IsA("LocalScript") or v:IsA("ModuleScript") then
+			table.insert(returntable, v)
+		end
+	end
+	return returntable
 end)
 
 getgenv().getnilinstances = newcclosure(function()
-    return nilinstances
+	return nilinstances
 end)
 
 getgenv().getsenv = newcclosure(function(script_instance)
@@ -272,7 +272,7 @@ getgenv().writefile = newcclosure(function(path, content)
 end)
 
 getgenv().makefolder = newcclosure(function(path)
-    Files[path] = {}
+	Files[path] = {}
 	Files[path .. '/'] = Files[path]
 end)
 
@@ -311,7 +311,7 @@ getgenv().delfile = newcclosure(function(path)
 	if type(f) == 'string' then Files[path] = nil end
 end)
 
-getgenv().listfiles = newcclosure((path)
+getgenv().listfiles = newcclosure(function(path)
 	if not path or path == '' then
 		local Files = {}
 		for i, v in pairs(Files) do
@@ -327,7 +327,7 @@ getgenv().listfiles = newcclosure((path)
 	return Files_2
 end)
 
-local VirtualInputManager = game:GetService("VirtualInputManager")
+	local VirtualInputManager = game:GetService("VirtualInputManager")
 
 getgenv().mouse1click = newcclosure(function()
 	VirtualInputManager:SendMouseButtonEvent(0, 0, 0, true, game, 1)
@@ -407,26 +407,26 @@ getgenv().set_hidden_property = sethiddenproperty
 getgenv().SetHiddenProperty = sethiddenproperty
 
 getgenv().isscriptable = newcclosure(function(Inst: Instance, Property: string)
-    local InstanceCache = ScriptableCache[Inst]
-    if InstanceCache then
-        local Value = InstanceCache[Property]
-        if Value ~= nil then
-            return Value
-        end
-    end
-    return _isscriptable(Inst, Property)
+	local InstanceCache = ScriptableCache[Inst]
+	if InstanceCache then
+		local Value = InstanceCache[Property]
+		if Value ~= nil then
+			return Value
+		end
+	end
+	return _isscriptable(Inst, Property)
 end)
 
 getgenv().is_scriptable = isscriptable
 getgenv().IsScriptable = isscriptable
 
 getgenv().setscriptable = newcclosure(function(Inst: Instance, Property: string, Scriptable: boolean)
-    local WasScriptable = _isscriptable(Inst, Property)
-    if ScriptableCache[Inst] == nil then
-        ScriptableCache[Inst] = {}
-    end
-    ScriptableCache[Inst][Property] = Scriptable
-    return WasScriptable
+	local WasScriptable = _isscriptable(Inst, Property)
+	if ScriptableCache[Inst] == nil then
+		ScriptableCache[Inst] = {}
+	end
+	ScriptableCache[Inst][Property] = Scriptable
+	return WasScriptable
 end)
 
 getgenv().set_scriptable = setscriptable
@@ -846,157 +846,157 @@ getgenv().lz4 = lz4
 local b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 
 getgenv().getc = function(str)
-    local sum = 0
-    for _, code in utf8.codes(str) do
-        sum = sum + code
-    end
-    return sum
+	local sum = 0
+	for _, code in utf8.codes(str) do
+		sum = sum + code
+	end
+	return sum
 end
 
 getgenv().str2hexa = function(a)
-    return string.gsub(
-        a,
-        ".",
-        function(b)
-            return string.format("%02x", string.byte(b))
-        end
-    )
+	return string.gsub(
+		a,
+		".",
+		function(b)
+			return string.format("%02x", string.byte(b))
+		end
+	)
 end
 
 getgenv().num2s = function(c, d)
-    local a = ""
-    for e = 1, d do
-        local f = c % 256
-        a = string.char(f) .. a
-        c = (c - f) / 256
-    end
-    return a
+	local a = ""
+	for e = 1, d do
+		local f = c % 256
+		a = string.char(f) .. a
+		c = (c - f) / 256
+	end
+	return a
 end
 
 getgenv().s232num = function(a, e)
-    local d = 0
-    for g = e, e + 3 do
-        d = d * 256 + string.byte(a, g)
-    end
-    return d
+	local d = 0
+	for g = e, e + 3 do
+		d = d * 256 + string.byte(a, g)
+	end
+	return d
 end
 
 getgenv().preproc = function(h, i)
-    local j = 64 - (i + 9) % 64
-    i = num2s(8 * i, 8)
-    h = h .. "\128" .. string.rep("\0", j) .. i
-    assert(#h % 64 == 0)
-    return h
+	local j = 64 - (i + 9) % 64
+	i = num2s(8 * i, 8)
+	h = h .. "\128" .. string.rep("\0", j) .. i
+	assert(#h % 64 == 0)
+	return h
 end
 
 getgenv().k = function(h, e, l)
-    local m = {}
-    local n = {
-        0x428a2f98,
-        0x71374491,
-        0xb5c0fbcf,
-        0xe9b5dba5,
-        0x3956c25b,
-        0x59f111f1,
-        0x923f82a4,
-        0xab1c5ed5,
-        0xd807aa98,
-        0x12835b01,
-        0x243185be,
-        0x550c7dc3,
-        0x72be5d74,
-        0x80deb1fe,
-        0x9bdc06a7,
-        0xc19bf174,
-        0xe49b69c1,
-        0xefbe4786,
-        0x0fc19dc6,
-        0x240ca1cc,
-        0x2de92c6f,
-        0x4a7484aa,
-        0x5cb0a9dc,
-        0x76f988da,
-        0x983e5152,
-        0xa831c66d,
-        0xb00327c8,
-        0xbf597fc7,
-        0xc6e00bf3,
-        0xd5a79147,
-        0x06ca6351,
-        0x14292967,
-        0x27b70a85,
-        0x2e1b2138,
-        0x4d2c6dfc,
-        0x53380d13,
-        0x650a7354,
-        0x766a0abb,
-        0x81c2c92e,
-        0x92722c85,
-        0xa2bfe8a1,
-        0xa81a664b,
-        0xc24b8b70,
-        0xc76c51a3,
-        0xd192e819,
-        0xd6990624,
-        0xf40e3585,
-        0x106aa070,
-        0x19a4c116,
-        0x1e376c08,
-        0x2748774c,
-        0x34b0bcb5,
-        0x391c0cb3,
-        0x4ed8aa4a,
-        0x5b9cca4f,
-        0x682e6ff3,
-        0x748f82ee,
-        0x78a5636f,
-        0x84c87814,
-        0x8cc70208,
-        0x90befffa,
-        0xa4506ceb,
-        0xbef9a3f7,
-        0xc67178f2
-    }
-    for g = 1, 16 do
-        m[g] = s232num(h, e + (g - 1) * 4)
-    end
-    for g = 17, 64 do
-        local o = m[g - 15]
-        local p = bit32.bxor(bit32.rrotate(o, 7), bit32.rrotate(o, 18), bit32.rshift(o, 3))
-        o = m[g - 2]
-        local q = bit32.bxor(bit32.rrotate(o, 17), bit32.rrotate(o, 19), bit32.rshift(o, 10))
-        m[g] = (m[g - 16] + p + m[g - 7] + q) % 2 ^ 32
-    end
-    local r, s, b, t, u, v, w, x = l[1], l[2], l[3], l[4], l[5], l[6], l[7], l[8]
-    for e = 1, 64 do
-        local p = bit32.bxor(bit32.rrotate(r, 2), bit32.rrotate(r, 13), bit32.rrotate(r, 22))
-        local y = bit32.bxor(bit32.band(r, s), bit32.band(r, b), bit32.band(s, b))
-        local z = (p + y) % 2 ^ 32
-        local q = bit32.bxor(bit32.rrotate(u, 6), bit32.rrotate(u, 11), bit32.rrotate(u, 25))
-        local A = bit32.bxor(bit32.band(u, v), bit32.band(bit32.bnot(u), w))
-        local B = (x + q + A + n[e] + m[e]) % 2 ^ 32
-        x = w
-        w = v
-        v = u
-        u = (t + B) % 2 ^ 32
-        t = b
-        b = s
-        s = r
-        r = (B + z) % 2 ^ 32
-    end
-    l[1] = (l[1] + r) % 2 ^ 32
-    l[2] = (l[2] + s) % 2 ^ 32
-    l[3] = (l[3] + b) % 2 ^ 32
-    l[4] = (l[4] + t) % 2 ^ 32
-    l[5] = (l[5] + u) % 2 ^ 32
-    l[6] = (l[6] + v) % 2 ^ 32
-    l[7] = (l[7] + w) % 2 ^ 32
-    l[8] = (l[8] + x) % 2 ^ 32
+	local m = {}
+	local n = {
+		0x428a2f98,
+		0x71374491,
+		0xb5c0fbcf,
+		0xe9b5dba5,
+		0x3956c25b,
+		0x59f111f1,
+		0x923f82a4,
+		0xab1c5ed5,
+		0xd807aa98,
+		0x12835b01,
+		0x243185be,
+		0x550c7dc3,
+		0x72be5d74,
+		0x80deb1fe,
+		0x9bdc06a7,
+		0xc19bf174,
+		0xe49b69c1,
+		0xefbe4786,
+		0x0fc19dc6,
+		0x240ca1cc,
+		0x2de92c6f,
+		0x4a7484aa,
+		0x5cb0a9dc,
+		0x76f988da,
+		0x983e5152,
+		0xa831c66d,
+		0xb00327c8,
+		0xbf597fc7,
+		0xc6e00bf3,
+		0xd5a79147,
+		0x06ca6351,
+		0x14292967,
+		0x27b70a85,
+		0x2e1b2138,
+		0x4d2c6dfc,
+		0x53380d13,
+		0x650a7354,
+		0x766a0abb,
+		0x81c2c92e,
+		0x92722c85,
+		0xa2bfe8a1,
+		0xa81a664b,
+		0xc24b8b70,
+		0xc76c51a3,
+		0xd192e819,
+		0xd6990624,
+		0xf40e3585,
+		0x106aa070,
+		0x19a4c116,
+		0x1e376c08,
+		0x2748774c,
+		0x34b0bcb5,
+		0x391c0cb3,
+		0x4ed8aa4a,
+		0x5b9cca4f,
+		0x682e6ff3,
+		0x748f82ee,
+		0x78a5636f,
+		0x84c87814,
+		0x8cc70208,
+		0x90befffa,
+		0xa4506ceb,
+		0xbef9a3f7,
+		0xc67178f2
+	}
+	for g = 1, 16 do
+		m[g] = s232num(h, e + (g - 1) * 4)
+	end
+	for g = 17, 64 do
+		local o = m[g - 15]
+		local p = bit32.bxor(bit32.rrotate(o, 7), bit32.rrotate(o, 18), bit32.rshift(o, 3))
+		o = m[g - 2]
+		local q = bit32.bxor(bit32.rrotate(o, 17), bit32.rrotate(o, 19), bit32.rshift(o, 10))
+		m[g] = (m[g - 16] + p + m[g - 7] + q) % 2 ^ 32
+	end
+	local r, s, b, t, u, v, w, x = l[1], l[2], l[3], l[4], l[5], l[6], l[7], l[8]
+	for e = 1, 64 do
+		local p = bit32.bxor(bit32.rrotate(r, 2), bit32.rrotate(r, 13), bit32.rrotate(r, 22))
+		local y = bit32.bxor(bit32.band(r, s), bit32.band(r, b), bit32.band(s, b))
+		local z = (p + y) % 2 ^ 32
+		local q = bit32.bxor(bit32.rrotate(u, 6), bit32.rrotate(u, 11), bit32.rrotate(u, 25))
+		local A = bit32.bxor(bit32.band(u, v), bit32.band(bit32.bnot(u), w))
+		local B = (x + q + A + n[e] + m[e]) % 2 ^ 32
+		x = w
+		w = v
+		v = u
+		u = (t + B) % 2 ^ 32
+		t = b
+		b = s
+		s = r
+		r = (B + z) % 2 ^ 32
+	end
+	l[1] = (l[1] + r) % 2 ^ 32
+	l[2] = (l[2] + s) % 2 ^ 32
+	l[3] = (l[3] + b) % 2 ^ 32
+	l[4] = (l[4] + t) % 2 ^ 32
+	l[5] = (l[5] + u) % 2 ^ 32
+	l[6] = (l[6] + v) % 2 ^ 32
+	l[7] = (l[7] + w) % 2 ^ 32
+	l[8] = (l[8] + x) % 2 ^ 32
 end
 
 getgenv().crypt = {
-    base64encode = function(data)
-        return (data:gsub('.', function(x) 
+	base64encode = function(data)
+		return (data:gsub('.', function(x) 
 			local r,b='',x:byte()
 			for i=8,1,-1 do r=r..(b%2^i-b%2^(i-1)>0 and '1' or '0') end
 			return r
@@ -1006,9 +1006,9 @@ getgenv().crypt = {
 			for i=1,6 do c=c+(x:sub(i,i)=='1' and 2^(6-i) or 0) end
 			return b64:sub(c+1,c+1)
 		end)..({'','==','='})[#data%3+1]
-    end,
-    base64decode = function(data)
-        data = data:gsub('[^'..b64..'=]', '')
+	end,
+	base64decode = function(data)
+		data = data:gsub('[^'..b64..'=]', '')
 		return (data:gsub('.', function(x)
 			if (x == '=') then return '' end
 			local r,f='',b64:find(x)-1
@@ -1020,15 +1020,15 @@ getgenv().crypt = {
 			for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
 			return string.char(c)
 		end)) 
-    end,
-    base64_decode = base64decode,
-    base64_encode = base64encode,
-    base64 = {
-     encode = base64encode,
-     decode = base64decode
-    },
-    encrypt = function(data, key, iv, mode)
-        assert(type(data) == "string", "Data must be a string")
+	end,
+	base64_decode = base64decode,
+	base64_encode = base64encode,
+	base64 = {
+		encode = base64encode,
+		decode = base64decode
+	},
+	encrypt = function(data, key, iv, mode)
+		assert(type(data) == "string", "Data must be a string")
 		assert(type(key) == "string", "Key must be a string")
 
 		mode = mode or "CBC"
@@ -1044,9 +1044,9 @@ getgenv().crypt = {
 
 		local encrypted = table.concat(res)
 		return crypt.base64encode(encrypted), iv
-    end,
-    decrypt = function(data, key, iv, mode)
-        assert(type(data) == "string", "Data must be a string")
+	end,
+	decrypt = function(data, key, iv, mode)
+		assert(type(data) == "string", "Data must be a string")
 		assert(type(key) == "string", "Key must be a string")
 		assert(type(iv) == "string", "IV must be a string")
 
@@ -1062,31 +1062,31 @@ getgenv().crypt = {
 		end
 
 		return table.concat(res)
-    end,
-    generatebytes = function(size)
-        local bytes = table.create(size)
+	end,
+	generatebytes = function(size)
+		local bytes = table.create(size)
 
 		for i = 1, size do
 			bytes[i] = string.char(math.random(0, 255))
 		end
 
 		return crypt.base64encode(table.concat(bytes))
-    end,
-    generatekey = function()
-        return crypt.generatebytes(32)
-    end,
-    hash = function(h)
-        h = preproc(h, #h)
-        local l = {0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19}
-        for e = 1, #h, 64 do
-            k(h, e, l)
-        end
-        return str2hexa(
-            num2s(l[1], 4) ..
-                num2s(l[2], 4) ..
-                    num2s(l[3], 4) .. num2s(l[4], 4) .. num2s(l[5], 4) .. num2s(l[6], 4) .. num2s(l[7], 4) .. num2s(l[8], 4)
-        )
-    end
+	end,
+	generatekey = function()
+		return crypt.generatebytes(32)
+	end,
+	hash = function(h)
+		h = preproc(h, #h)
+		local l = {0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19}
+		for e = 1, #h, 64 do
+			k(h, e, l)
+		end
+		return str2hexa(
+			num2s(l[1], 4) ..
+				num2s(l[2], 4) ..
+				num2s(l[3], 4) .. num2s(l[4], 4) .. num2s(l[5], 4) .. num2s(l[6], 4) .. num2s(l[7], 4) .. num2s(l[8], 4)
+		)
+	end
 }
 
 local DrawingLib = {}
@@ -1834,11 +1834,11 @@ ExecuteButton.BackgroundColor3 = Color3.fromRGB(70, 70, 70)
 ExecuteButton.TextColor3 = Color3.fromRGB(255, 255, 255)
 
 ExecuteButton.MouseButton1Click:Connect(function()
-    local scriptText = TextBox.Text
-    local func, err = loadstring(scriptText)
-    if func then
-        func()
-    else
-        warn("Error executing script: " .. err)
-    end
+	local scriptText = TextBox.Text
+	local func, err = loadstring(scriptText)
+	if func then
+		func()
+	else
+		warn("Error executing script: " .. err)
+	end
 end)
